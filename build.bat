21fe1e6ad01352ea5311bac2c25e06cb4cf500a9
Joachim Bauch <mail@joachim-bauch.de>
2014-01-07 16:52:16 +0100

    Also compile "libde265".

--- build.bat
+++ build.bat
@@ -2,10 +2,24 @@
 
 call "%VS120COMNTOOLS%vsvars32.bat"
 
+cd libde265
+cmd /c build.bat x86
+cd ..
+pexports libde265\libde265\libde265.dll > libde265\libde265\libde265.def
+dlltool -d libde265\libde265\libde265.def -l libde265\libde265\libde265.a
 sh build_ffmpeg.sh x86
+copy /y libde265\bin_x86\libde265.dll bin_Win32
+copy /y libde265\bin_x86\libde265.dll bin_Win32d
 devenv LAVFilters.sln /Rebuild "Release|Win32"
 
+cd libde265
+cmd /c build.bat x64
+cd ..
+pexports libde265\libde265\libde265.dll > libde265\libde265\libde265.def
+dlltool -d libde265\libde265\libde265.def -l libde265\libde265\libde265.a
 sh build_ffmpeg.sh x64
+copy /y libde265\bin_x64\libde265.dll bin_x64
+copy /y libde265\bin_x64\libde265.dll bin_x64d
 devenv LAVFilters.sln /Rebuild "Release|x64"
 
 PAUSE
