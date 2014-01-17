836fb017a1dbd79e16406d20fe76ab1a3654cfec
Joachim Bauch <mail@joachim-bauch.de>
2014-01-17 17:34:48 +0100

    Register libde265 codec from inside the LAVVideo filter.
    That way we can use the unmodified ffmpeg code without having to adjust our patches.

--- build.bat
+++ build.bat
@@ -8,7 +8,6 @@ cmd /c build.bat x86
 cd ..
 pexports libde265\libde265\libde265.dll > libde265\libde265\libde265.def
 dlltool -d libde265\libde265\libde265.def -l libde265\libde265\libde265.a
-sh build_ffmpeg.sh x86
 copy /y libde265\bin_x86\libde265.dll bin_Win32
 copy /y libde265\bin_x86\libde265.dll bin_Win32d
 MSBuild.exe LAVFilters.sln /nologo /m /t:Rebuild /property:Configuration=Release;Platform=Win32
@@ -20,7 +19,6 @@ cmd /c build.bat x64
 cd ..
 pexports libde265\libde265\libde265.dll > libde265\libde265\libde265.def
 dlltool -d libde265\libde265\libde265.def -l libde265\libde265\libde265.a
-sh build_ffmpeg.sh x64
 copy /y libde265\bin_x64\libde265.dll bin_x64
 copy /y libde265\bin_x64\libde265.dll bin_x64d
 MSBuild.exe LAVFilters.sln /nologo /m /t:Rebuild /property:Configuration=Release;Platform=x64
