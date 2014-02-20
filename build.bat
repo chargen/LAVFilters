e18561d8193b2a2d0a2f3996e6caa2f4e693fff5
Joachim Bauch <bauch@struktur.de>
2014-02-20 16:19:08 +0100

    Updated to libde265 0.5 (+ latest Windows fixes).

--- build.bat
+++ build.bat
@@ -10,6 +10,7 @@ copy /y libde265\bin_x86\libde265.dll bin_Win32
 copy /y libde265\bin_x86\libde265.dll bin_Win32d
 copy /y libde265\bin_x86\lib\libde265.lib bin_Win32\lib
 copy /y libde265\bin_x86\lib\libde265.lib bin_Win32d\lib
+copy /y libde265\extra\libde265\de265-version.h libde265\libde265
 devenv LAVFilters.sln /Rebuild "Release|Win32"
 
 sh build_ffmpeg.sh x64
@@ -20,6 +21,7 @@ copy /y libde265\bin_x64\libde265.dll bin_x64
 copy /y libde265\bin_x64\libde265.dll bin_x64d
 copy /y libde265\bin_x64\lib\libde265.lib bin_x64\lib
 copy /y libde265\bin_x64\lib\libde265.lib bin_x64d\lib
+copy /y libde265\extra\libde265\de265-version.h libde265\libde265
 devenv LAVFilters.sln /Rebuild "Release|x64"
 
 PAUSE
