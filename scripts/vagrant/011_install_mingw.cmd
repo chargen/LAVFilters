2047efe0714b2f6ce46b87a80b8cf4cf9f1dc575
Joachim Bauch <bauch@struktur.de>
2015-07-20 17:55:52 +0200

    Fixed use of variables in MSYS/MinGW installer.

--- scripts/vagrant/011_install_mingw.cmd
+++ scripts/vagrant/011_install_mingw.cmd
@@ -1,12 +1,10 @@
 @echo off
 
+set MSYS_FILENAME=MSYS_MinGW-w64_GCC_493_x86-x64.7z
 if not exist "C:\MSYS" (
     echo Fetching MSYS/MinGW...
-    setlocal
-    set MSYS_FILENAME=MSYS_MinGW-w64_GCC_493_x86-x64.7z
     PowerShell Invoke-WebRequest -Uri http://xhmikosr.1f0.de/tools/msys/%MSYS_FILENAME% -OutFile "C:\Users\vagrant\%MSYS_FILENAME%"
     C:\vagrant\scripts\vagrant\7za.exe x -y -oC:\ "C:\Users\vagrant\%MSYS_FILENAME%" > NUL
-    endlocal
     SETX PATH "%PATH%;C:\MSYS\bin;C:\MSYS\mingw\bin" > NUL
     SET "PATH=%PATH%;C:\MSYS\bin;C:\MSYS\mingw\bin" > NUL
 ) else (
