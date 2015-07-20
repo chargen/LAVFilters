fe30d2acda62aeb331836f2f1d12135454401607
Joachim Bauch <bauch@struktur.de>
2015-07-20 18:47:42 +0200

    Optionally code-sign files.

--- scripts/vagrant/040_package.cmd
+++ scripts/vagrant/040_package.cmd
@@ -6,6 +6,11 @@ if exist libde265-*.exe (
     del /q libde265-*.exe > NUL
 )
 
+if exist "C:\vagrant\scripts\vagrant\sign_files.cmd" (
+    call "C:\vagrant\scripts\vagrant\sign_files.cmd" "C:\Users\vagrant\LAVFilters\bin_Win32"
+    call "C:\vagrant\scripts\vagrant\sign_files.cmd" "C:\Users\vagrant\LAVFilters\bin_x64"
+)
+
 "C:\Program Files (x86)\Inno Setup 5\ISCC.exe" LAVFilters.iss
 
 set TODAY=
@@ -14,5 +19,8 @@ if not exist "C:\vagrant\build_result\%TODAY:~0,8%-%TODAY:~8,6%" (
     mkdir C:\vagrant\build_result\%TODAY:~0,8%-%TODAY:~8,6%
 )
 copy /y libde265-*.exe C:\vagrant\build_result\%TODAY:~0,8%-%TODAY:~8,6% >NUL
+if exist "C:\vagrant\scripts\vagrant\sign_files.cmd" (
+    call "C:\vagrant\scripts\vagrant\sign_files.cmd" "C:\vagrant\build_result\%TODAY:~0,8%-%TODAY:~8,6%"
+)
 
 echo Done, installer copied to "build_result\%TODAY:~0,8%-%TODAY:~8,6%"
