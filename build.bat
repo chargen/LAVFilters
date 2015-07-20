2017f16b56f5366064bce14d4ccac5ec584c2dba
Joachim Bauch <bauch@struktur.de>
2015-07-20 14:36:22 +0200

    Update libde265 build script to use VS120COMNTOOLS.

--- build.bat
+++ build.bat
@@ -4,7 +4,9 @@ call "%VS120COMNTOOLS%vsvars32.bat"
 
 sh build_ffmpeg.sh x86 || EXIT /B 1
 cd libde265
+sed -i 's/VS110COMNTOOLS/VS120COMNTOOLS/g' build.bat
 cmd /c build.bat x86
+sed -i 's/VS120COMNTOOLS/VS110COMNTOOLS/g' build.bat
 cd ..
 copy /y libde265\bin_x86\libde265.dll bin_Win32
 copy /y libde265\bin_x86\lib\libde265.lib bin_Win32\lib
@@ -15,7 +17,9 @@ MSBuild.exe LAVFilters.sln /nologo /m /t:Rebuild /property:Configuration=Release
 
 sh build_ffmpeg.sh x64 || EXIT /B 1
 cd libde265
+sed -i 's/VS110COMNTOOLS/VS120COMNTOOLS/g' build.bat
 cmd /c build.bat x64
+sed -i 's/VS120COMNTOOLS/VS110COMNTOOLS/g' build.bat
 cd ..
 copy /y libde265\bin_x64\libde265.dll bin_x64
 copy /y libde265\bin_x64\lib\libde265.lib bin_x64\lib
